734691d9bdfa19e0176a86032c651c492f55f8eb
Lakrids7 <Krelle757@gmail.com>
2024-03-06 11:35:28 +0100

    Ups hihi
    koden var lidt messed up

--- python/library/FaceDetectionMethods.py
+++ python/library/FaceDetectionMethods.py
@@ -5,8 +5,10 @@ from library.MouthOpeningArea import calculate_mouth_opening_area
 from library.HeadAngle import calculate_default_face_angle
 
 
-# Method that returns the facial landmarks and the frame with the landmarks drawn on it, needs the video capture, the face cascade and the ladnmark predictor
 def detect_faces_and_landmarks(source, face_cascade, predictor, is_image=False):
+    """Returns the facial landmarks and the frame with the landmarks drawn on it, needs the video
+    capture, the face cascade and the landmark predictor"""
+
     # Read a frame from video capture or use the provided image
     if is_image:
         frame = source
@@ -37,44 +39,9 @@ def detect_faces_and_landmarks(source, face_cascade, predictor, is_image=False):
             cv2.putText(frame, str(n), (x, y), cv2.FONT_HERSHEY_SIMPLEX, 0.3, (0, 255, 0), 1)
         return landmarks, frame
 
+
 def initialize_face_and_landmark_data():
-    face_cascade = cv2.CascadeClassifier('HaarCascadeData/haarcascade_frontalface_default.xml')
-    predictor_path = "FacialLandmarkData/shape_predictor_68_face_landmarks.dat"
-    predictor = dlib.shape_predictor(predictor_path)
-    return face_cascade, predictor
-
-if __name__ == "__main__":
-    # Initialize video capture with DirectShow API
-    cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
-    face_cascade, predictor = initialize_face_and_landmark_data()
-
-    while True:
-        # Read frame and detect landmarks
-        face_landmarks, frame = detect_faces_and_landmarks(cap, face_cascade, predictor)
-
-        # Process the detected landmarks
-        if face_landmarks is not None:
-            lip_area = calculate_mouth_opening_area(face_landmarks)
-            print(lip_area)
-
-            default_face_angle = calculate_default_face_angle(face_landmarks)
-            print(f"Default face ratio is {default_face_angle}")
-        else:
-            print("No facial landmarks detected.")
-
-        # Display the resulting frame
-        cv2.imshow('Facial Landmarks', frame)
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
-
-    # Cleanup
-    cap.release()
-    cv2.destroyAllWindows()
-
-
-
-#Method to initialize the face cascade and the landmark predictor (This is now a seperate method from the landmark detection because we only want to do it once)
-def initialize_face_and_landmark_data():
+    """Initializes the face cascade and the landmark predictor and returns them as a tuple."""
     # Load Haar Cascade for face detection
     face_cascade = cv2.CascadeClassifier('HaarCascadeData/haarcascade_frontalface_default.xml')
     # Initialize Dlib's facial landmark predictor
