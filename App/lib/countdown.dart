5ef1f8bc078a7a6b145611ddd67290c3c19228c9
marchusl <marchusl@gmail.com>
2024-04-17 11:50:30 +0200

    works again

--- App/lib/countdown.dart
+++ App/lib/countdown.dart
@@ -1,5 +1,12 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:dart/Assets/circle.dart';
+import 'dart:typed_data';
+import 'package:camera/camera.dart';
+import 'package:flutter/services.dart';
+import 'package:auto_size_text/auto_size_text.dart';
+import 'camera_recording.dart';
+import 'network_client.dart';
 
 void main() {
   runApp(StartCountdown());
@@ -42,8 +49,10 @@ class _CountdownScreenState extends State<CountdownScreen> {
     super.dispose();
   }
 
-  void startTimer() {
+
+  void startTimer(int duration) {
     const oneSecond = Duration(seconds: 1);
+    _secondsRemaining = duration;
     _timer = Timer.periodic(oneSecond, (timer) {
       setState(() {
         if (_secondsRemaining <= 0) {
@@ -55,85 +64,115 @@ class _CountdownScreenState extends State<CountdownScreen> {
     });
   }
 
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Color(0xFFEBEEF3),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            _isCountingDown
-                ? Text(
-              'Time Remaining:',
-              style: TextStyle(fontSize: 35, fontWeight: FontWeight.bold),
-            )
-                : Text(
-              'Press the button to start',
-              style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
-            ),
-            SizedBox(height: 10),
-            _isCountingDown
-                ? Text(
-              '$_secondsRemaining seconds',
-              style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
-            )
-                : SizedBox(),
-            SizedBox(height: 20),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                SizedBox(
-                  width: 150, // Specify the width of the button
-                  child: TextButton(
-                    style: ButtonStyle(
-                      backgroundColor: MaterialStateProperty.all<Color>(const Color(0xFF153867)),
+    double mWidth = MediaQuery.of(context).size.width;
+    double mHeight = MediaQuery.of(context).size.height;
+    double circleHeight = (mHeight / 5) * 2;
+    double cameraWidth = (mWidth / 7) * 6;
+    double cameraHeight = (mHeight / 12) * 8;
+    double buttonPosW = (mWidth / 7);
+    double buttonPosH = (mHeight / 10);
+    double buttonWidth = (mWidth / 4);
+    double buttonHeight = (mHeight / 16);
+    return Material(
+        child: Stack(
+            children: [
+              Circle(mWidth: mWidth, circleHeight: circleHeight,),
+              Positioned(
+                left: (mWidth - cameraWidth) / 2,
+                top: circleHeight / 2 + 10,
+                child: Container(
+                  height: cameraHeight,
+                  width: cameraWidth,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: const BorderRadius.only(
+                        topLeft: Radius.circular(10),
+                        topRight: Radius.circular(10),
+                        bottomLeft: Radius.circular(10),
+                        bottomRight: Radius.circular(10)
                     ),
-                    onPressed: () {
-                      if (!_isCountingDown) {
-                        setState(() {
-                          _isCountingDown = true;
-                        });
-                        startTimer();
-                      }
-                    },
-                    child: Text(
-                      'Start \nCountdown',
-                      textAlign: TextAlign.center,
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 16,
-                        fontFamily: 'Inter',
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.grey.withOpacity(0.5),
+                        spreadRadius: 5,
+                        blurRadius: 7,
+                        offset: Offset(0, 3), // changes position of shadow
                       ),
+                    ],
+                  ),
+                ),
+              ),
+              Positioned(
+                right: buttonPosW,
+                top: mHeight - buttonPosH,
+                child: TextButton(
+                  style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all<Color>(const Color(0xFF153867)),
+                    minimumSize: MaterialStateProperty.all(Size(buttonWidth, buttonHeight)),
+                    maximumSize: MaterialStateProperty.all(Size(buttonWidth, buttonHeight)),
+                  ),
+                  onPressed: () {
+                    // Start the countdown with the desired duration
+                    startTimer(10); // Example: Start the countdown for 10 seconds
+                  },
+                  child: const Text(
+                    'Continue \n anyway',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 16,
+                      fontFamily: 'Inter',
+                      fontWeight: FontWeight.w400,
+                      height: 0,
                     ),
                   ),
                 ),
-                SizedBox(width: 20), // Add space between the buttons
-                SizedBox(
-                  width: 150, // Specify the width of the button
-                  child: TextButton(
-                    style: ButtonStyle(
-                      backgroundColor: MaterialStateProperty.all<Color>(const Color(0xFF153867)),
-                    ),
-                    onPressed: () {
-                      // Navigate to a different page
-                    },
-                    child: Text(
-                      'Go to \nPage',
-                      textAlign: TextAlign.center,
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 16,
-                        fontFamily: 'Inter',
-                      ),
+              ),
+              Positioned(
+                left: buttonPosW,
+                top: mHeight - buttonPosH,
+                child: TextButton(
+                  style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all<Color>(
+                        const Color(0xFF153867)),
+                    minimumSize: MaterialStateProperty.all(
+                        Size(buttonWidth, buttonHeight,)),
+                    maximumSize: MaterialStateProperty.all(
+                        Size(buttonWidth, buttonHeight,)),
+                  ),
+                  onPressed: () {
+
+                  },
+                  child: const Text(
+                    'Repeat \ninstructions',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 16,
+                      fontFamily: 'Inter',
+                      fontWeight: FontWeight.w400,
+                      height: 0,
                     ),
                   ),
                 ),
-              ],
-            ),
-          ],
-        ),
-      ),
-    );
+              ),
+              Column(
+                children: <Widget>[
+                  SizedBox(height: 300,width: 400),
+                  Text(
+                    '$_secondsRemaining',
+                    style: TextStyle(
+                      fontSize: 225,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black.withOpacity(0.5), // Set text color with opacity
+                    ),
+                  ),
+                ],
+              )
+    ]));
   }
+
 }
