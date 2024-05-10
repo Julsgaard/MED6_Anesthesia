a6760cdaf16a9df6b03041282ae46d261ed4532a
Lakrids7 <Krelle757@gmail.com>
2024-05-10 14:08:10 +0200

    Exercise animations better synced with what python expects

--- python/main.py
+++ python/main.py
@@ -44,6 +44,8 @@ if __name__ == '__main__':
 
         # Find the state for the image path
         new_state = functions.find_state_for_image_path(image_path)
+
+        #CHECK TO RESET THE TIMER IF THE STATE CHANGES
         if current_state != new_state:
             current_state = new_state
             timer.reset()  # Reset timer when state changes
@@ -57,84 +59,98 @@ if __name__ == '__main__':
 
         # Check the state and run the appropriate functions
         if current_state == 'Mouth Opening':
+            # Timer
+            time_elapsed = timer.elapsed_time()
+            print(f"Time elapsed: {time_elapsed}")
+            #Mouth opening state er lidt ligegyldigt om de åbner munden 'for sent', det vigtigste er at de har munden lukket når exercise starter og det må vi gå ud fra de har
+            if time_elapsed > 2: #Give them X amount of time to open their mouth
 
-            # Display the image
-            display_image_queue.put(frame)
+                # Display the image
+                display_image_queue.put(frame)
 
-            if face_landmarks:
-                # Calculate mouth opening ratio
-                lip_distance = MouthOpeningRatio.lip_distance_monitor(face_landmarks)
-                if lip_distance:
-                    print(f"Lip Distance Increase: {lip_distance:.2f}%.")
-                    functions.save_results_to_file(image_path, f"Lip Distance Increase: {lip_distance:.2f}%.")
+                if face_landmarks:
+                    # Calculate mouth opening ratio
+                    lip_distance = MouthOpeningRatio.lip_distance_monitor(face_landmarks)
+                    if lip_distance:
+                        print(f"Lip Distance Increase: {lip_distance:.2f}%.")
+                        functions.save_results_to_file(image_path, f"Lip Distance Increase: {lip_distance:.2f}%.")
 
 
         elif current_state == 'Mallampati':
-            if face_landmarks:
-                cropped_image = MouthCrop.crop_mouth_region(frame, face_landmarks)
+            # Timer
+            time_elapsed = timer.elapsed_time()
+            print(f"Time elapsed: {time_elapsed}")
+            if time_elapsed > 10: #Give them X amount of time to open their mouth(?)
+            # Ved mallampati går der cirka 10 sekunder fra at exercise starter til at avatar siger "now, open your mouth" så ja
+                if face_landmarks:
+                    cropped_image = MouthCrop.crop_mouth_region(frame, face_landmarks)
 
-                # Prepare the image
-                transformed_image = mallampati_image_prep.prepare_mallampati_image_for_loader(cropped_image,
-                                                                                              image_pixel_size=64)
+                    # Prepare the image
+                    transformed_image = mallampati_image_prep.prepare_mallampati_image_for_loader(cropped_image,
+                                                                                                  image_pixel_size=64)
 
-                # Add a dimension to the image
-                image_with_extra_dim = transformed_image.unsqueeze(0)
+                    # Add a dimension to the image
+                    image_with_extra_dim = transformed_image.unsqueeze(0)
 
-                # Display the image of the mouth
-                display_image_queue.put(image_with_extra_dim[0].numpy().transpose((1, 2, 0)))
+                    # Display the image of the mouth
+                    display_image_queue.put(image_with_extra_dim[0].numpy().transpose((1, 2, 0)))
 
-                # Run the model
-                predicted = mallampati_CNN_run_model.run_predictions_on_image(model, image_with_extra_dim, device)
+                    # Run the model
+                    predicted = mallampati_CNN_run_model.run_predictions_on_image(model, image_with_extra_dim, device)
 
-                if predicted == 0:
-                    print("Mallampati Class 1 or 2")
+                    if predicted == 0:
+                        print("Mallampati Class 1 or 2")
 
-                    # Save the prediction to a file
-                    functions.save_results_to_file(image_path, "Mallampati Class 1 or 2")
+                        # Save the prediction to a file
+                        functions.save_results_to_file(image_path, "Mallampati Class 1 or 2")
 
-                    # Add prediction to list
-                    predictions.append(0)
+                        # Add prediction to list
+                        predictions.append(0)
 
-                elif predicted == 1:
-                    print("Mallampati Class 3 or 4")
+                    elif predicted == 1:
+                        print("Mallampati Class 3 or 4")
 
-                    # Save the prediction to a file
-                    functions.save_results_to_file(image_path, "Mallampati Class 3 or 4")
+                        # Save the prediction to a file
+                        functions.save_results_to_file(image_path, "Mallampati Class 3 or 4")
 
-                    # Add prediction to list
-                    predictions.append(1)
+                        # Add prediction to list
+                        predictions.append(1)
 
-                # After 20 predictions, take the mean of the predictions
-                if len(predictions) >= 20:
-                    mean_prediction = sum(predictions) / len(predictions)
+                    # After 20 predictions, take the mean of the predictions
+                    if len(predictions) >= 20:
+                        mean_prediction = sum(predictions) / len(predictions)
 
-                    if mean_prediction < 0.5:
-                        print(f"User is predicted to be Mallampati Class 1 or 2 | Mean: {mean_prediction}")
-                    else:
-                        print(f"User is predicted to be Mallampati Class 3 or 4 | Mean: {mean_prediction}")
+                        if mean_prediction < 0.5:
+                            print(f"User is predicted to be Mallampati Class 1 or 2 | Mean: {mean_prediction}")
+                        else:
+                            print(f"User is predicted to be Mallampati Class 3 or 4 | Mean: {mean_prediction}")
 
-                #TODO: We might need a state for Mallampati done, so we can save the final prediction to a file.
+                    #TODO: We might need a state for Mallampati done, so we can save the final prediction to a file.
 
         elif current_state == 'Neck Movement':
-            nose_tracker, chin_tracker, frame, head_angle = Tracker.add_chin_and_nose_tracker(frame, face_landmarks,
-                                                                                              nose_tracker,
-                                                                                              chin_tracker)
-            if head_angle:
-                print(f"Head Angle in degrees: {head_angle}")
+            # Timer
+            time_elapsed = timer.elapsed_time()
+            print(f"Time elapsed: {time_elapsed}")
+            if time_elapsed > 2: #Give them X amount of time to open their mouth(?)
+                # Head tilt er det også bare vigtigt at de har head i neutral position i starten. Ellers er det vigtigste at vi ved hvornår det er forward tilt og back tilt
+                nose_tracker, chin_tracker, frame, head_angle = Tracker.add_chin_and_nose_tracker(frame, face_landmarks,
+                                                                                                  nose_tracker,
+                                                                                                  chin_tracker)
+                if head_angle:
+                    print(f"Head Angle in degrees: {head_angle}")
 
-                time_elapsed = timer.elapsed_time()
-                print(f"Time elapsed: {time_elapsed}")
-                if time_elapsed < 10:
-                    functions.save_results_to_file(image_path, f"Upper head angle: {head_angle}")
 
-                    #print(f"Stored as Upper Head Angle: {upper_head_angle}")
-                else:
-                    lower_head_angle = head_angle
-                    functions.save_results_to_file(image_path, f"Lower head angle: {head_angle}")
+                    if time_elapsed < 35: #Der går cirka 35 sekunder fra at exercise starter til at avatar siger "now, tilt your head forward"
+                        functions.save_results_to_file(image_path, f"Upper head angle: {head_angle}")
 
-                    #print(f"Stored as Lower Head Angle: {lower_head_angle}")
+                        #print(f"Stored as Upper Head Angle: {upper_head_angle}")
+                    else:
+                        lower_head_angle = head_angle
+                        functions.save_results_to_file(image_path, f"Lower head angle: {head_angle}")
 
-            display_image_queue.put(frame)
+                        #print(f"Stored as Lower Head Angle: {lower_head_angle}")
+
+                display_image_queue.put(frame)
 
         elif current_state == 'Error State':
 
