12829d87a33758cb179f5e2b5788512230e8102e
Lakrids7 <Krelle757@gmail.com>
2024-05-20 08:42:13 +0200

    R studio project

---
+++ R/P6RScript.R
@@ -0,0 +1,13 @@
+#------- Libraries -------
+library(tidyverse)
+library(readbulk)
+library(magrittr)
+library(ggpubr)
+library(reshape2)
+library(Hmisc)
+library(pastecs)
+library(FSA)
+library(ggplot2)
+library(readr)
+
+
