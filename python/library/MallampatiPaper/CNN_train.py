03e0f5fdff0138af6051c6363ef60a7a13e9e088
Julsgaard <100912975+Julsgaard@users.noreply.github.com>
2024-06-13 22:26:05 +0200

    Update CNN_train.py

--- python/library/MallampatiPaper/CNN_train.py
+++ python/library/MallampatiPaper/CNN_train.py
@@ -32,14 +32,12 @@ valid_dir='Dataset/validation_data'  #所有训练数据Trian_all的20%
 #对训练集所需要做的预处理   
 train_transform=transforms.Compose([
     transforms.Resize(224),
-    transforms.CenterCrop(224),
     transforms.ToTensor(),
     transforms.Normalize((0.485,0.456,0.406),(0.229,0.224,0.225))
 ])
 #对验证集所需要做的预处理
 valid_transform=transforms.Compose([
     transforms.Resize(224),
-    transforms.CenterCrop(224),
     transforms.ToTensor(),
     transforms.Normalize((0.485,0.456,0.406),(0.229,0.224,0.225))
 ])
